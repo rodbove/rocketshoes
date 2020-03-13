50d8f3ef85e9ae4464ea36425caca83283a586d1
PooWoox <41601403+PooWoox@users.noreply.github.com>
2020-03-13 12:00:04 -0300

    Styled Home and Cart pages; Implemented Redux; Added Cart reducer and configured Reactotron

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
+
+import store from './store'
 
 function App() {
   return (
-    <h1>Hello Rocketshoes!</h1>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
