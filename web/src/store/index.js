96f7e85990927f0ac1ae969e4fc55db3b1536a31
PooWoox <41601403+PooWoox@users.noreply.github.com>
2020-03-18 12:41:56 -0300

    implemented React Hooks syntax on components and Redux

---
+++ web/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSaggaMiddleware from 'redux-saga';
+
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMonitor = process.env.NODE_ENV =='development'
+  ? console.tron.createSagaMonitor()
+  : null;
+
+const sagaMiddleware = createSaggaMiddleware({
+  sagaMonitor,
+});
+
+const enhancer = process.env.NODE_ENV == 'development'
+  ? compose(
+    console.tron.createEnhancer(),
+    applyMiddleware(sagaMiddleware)
+    )
+  : applyMiddleware(sagaMiddleware);
+
+const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
